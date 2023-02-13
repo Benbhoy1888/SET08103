6d123604770b651158175727aec77d425a6bf372
Andy Dickinson <dickinson.andrew@hotmail.co.uk>
2023-02-13 13:49:11 +0000

    .

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,6 +7,10 @@ import org.bson.Document;
 
 public class App
 {
+    /**
+     * description
+     * @param args parameters
+     */
     public static void main(String[] args)
     {
         // Connect to MongoDB
