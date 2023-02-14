6dbb03013a73e6df3f83499624da69ebdcb6523c
Tim Honisett <40595819@live.napier.ac.uk>
2023-02-14 15:41:13 +0000

    Correct github auto

---
+++ src/main/java/com/napier/sem/Employee.java
@@ -0,0 +1,54 @@
+package com.napier.sem;
+
+import java.sql.Statement;
+
+public class Employee {
+
+    /**
+     * This class creates Employee objects and has methods for running SQL queries against the employee table
+     *
+     */
+
+    /***
+     * Employee number
+     */
+    public int emp_no;
+
+    /**
+     * Employee's first name
+     */
+    public String first_name;
+
+    /**
+     * Employee's last name
+     */
+    public String last_name;
+
+    /**
+     * Employee's job title
+     */
+    public String title;
+
+    /**
+     * Employee's salary
+     */
+
+    public int salary;
+
+    /**
+     * Employee's current department
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public String manager;
+
+
+    /**
+     * @param ID employee id (int)
+     * @return first and last names
+     */
+
+}
