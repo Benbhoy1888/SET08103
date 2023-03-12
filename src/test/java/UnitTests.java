9974a5c6adf842b95c47065cc1e5d6acd508e907
Andy Dickinson <dickinson.andrew@hotmail.co.uk>
2023-03-12 11:43:50 +0000

    Main.yml updated to point at UnitTests file (currently empty)

---
+++ src/test/java/UnitTests.java
@@ -0,0 +1,15 @@
+import com.napier.sem.App;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Project Unit tests
+ * Run using Maven by opening Maven view ->
+ * Select "test" lifecycle stage
+ *
+ * Tests should be written in a separate file for each class.
+ * Ensure to add file to main.yml for Unit tests run line(s)
+ */
+public class UnitTests {
+
+}
