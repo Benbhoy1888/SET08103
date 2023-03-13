44e6bfb5205a225109b5d5e19d1f69213eccfc15
Andy Dickinson <dickinson.andrew@hotmail.co.uk>
2023-03-13 15:18:38 +0000

    Started working on countries report in meeting with team

--- src/main/java/com/napier/sem/Capital.java
+++ src/main/java/com/napier/sem/Capital.java
@@ -2,6 +2,5 @@ package com.napier.sem;
 
 public class Capital extends Population{
 
-    // This class inherits objects from Population class
-    // Used for capitals use case
+
 }
---
+++ src/main/java/com/napier/sem/Countries.java
@@ -0,0 +1,37 @@
+package com.napier.sem;
+
+/**
+ * Represents a country
+ */
+public class Countries {
+
+    /**
+     * Country code
+     */
+    public String code;
+
+    /**
+     * Country name
+     */
+    public String name;
+
+    /**
+     * Continent
+     */
+    public String continent;
+
+    /**
+     * Region
+     */
+    public String region;
+
+    /**
+     * Population
+     */
+    public int population;
+
+    /**
+     * Capital
+     */
+    public int capital;
+}
