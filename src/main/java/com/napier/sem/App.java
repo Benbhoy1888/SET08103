0b9ba3c122b6795f829e1d7e0b1262f8208fae9a
Andy Dickinson <dickinson.andrew@hotmail.co.uk>
2023-03-15 11:51:31 +0000

    All country reports finished (with exception of outputting to markdown file) - tests still require writting

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -25,68 +25,131 @@ public class App
         // Connect to database
         a.connect();
 
-        // Extract country information
-        ArrayList<Country> countries = a.getAllCountries("", "");
-        a.printCountries(countries);
-
+        ///////////////////// NO LONGER USED, keep here for now //////////////////
         // Creates menu for user - parses choice(s) and creates relevant report(s)
         //a.createMenu();
+
+
+        // Country reports
+
+        // Extract country information for world report
+        ArrayList<Country> worldCountries = a.getAllCountries("w", "");
+        // Extract country information for continent report
+        ArrayList<Country> continentCountries = a.getAllCountries("c", "Oceania");
+        //Extract country information for region report
+        ArrayList<Country> regionCountries = a.getAllCountries("r", "Western Europe");
+
+        // Print report for all countries in the world
+        System.out.println("Countries report (all, world):");
+        a.printCountries(worldCountries, -1);
+        // Print report for all countries in continent
+        System.out.println("Countries report (all, continent = 'Oceania'):");
+        a.printCountries(continentCountries, -1);
+        // Print report for all countries in region
+        System.out.println("Countries report (all, region = 'Western Europe'):");
+        a.printCountries(regionCountries, -1);
+        // Print report for top 5 most populated countries in world
+        System.out.println("Countries report (n = 5, world):");
+        a.printCountries(worldCountries, 5);
+        // Print report for top 8 most populated countries in continent
+        System.out.println("Countries report (n = 8, continent = 'Oceania'):");
+        a.printCountries(continentCountries, 8);
+        // Print report for top 3 most populated countries in region
+        System.out.println("Countries report (n = 3, region = 'Western Europe'):");
+        a.printCountries(regionCountries, 3);
+
 
         // Disconnect from database
         a.disconnect();
     }
 
     /**
-     * Prints a list of countries.
-     * @param countries The list of countries to print.
+     * Prints a list of countries
+     * @param countries The list of countries to print
+     * @param displayN number to display, -1 displays all
      */
-    public void printCountries(ArrayList<Country> countries)
+    public void printCountries(ArrayList<Country> countries, int displayN)
     {
-        // Print header
-        System.out.println(String.format("%-3s %-52s %-10s %-15s %-26s %-1s", "Code", "Name", "Population", "Continent", "Region", "Capital"));
-        // Loop over all employees in the list
-        for (Country country : countries)
-        {
-            String country_string =
-                    String.format("%-3s %-52s %-10s %-15s %-26s %-1s",
-                            country.code, country.name, country.population, country.continent, country.region, country.capital);
-            System.out.println(country_string);
+        // checks list exists
+        if(countries != null) {
+
+            // checks list is not empty
+            if(countries.size()<1){
+                System.out.println("No countries found\n");
+                return;
+            }
+
+            // Print header
+            System.out.println(String.format("%-4s %-53s %-15s %-27s %-12s %-36s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
+
+            // sets displayN to total number of countries in ArrayList if either disiplayN is set to -1 (display all)
+            // or displayN is greater than the number of countries
+            if(displayN>countries.size() || displayN==-1){
+                displayN = countries.size();
+            }
+
+            // Loop over all countries in the list
+            for (int i=0; i<displayN;i++) {
+                Country country;
+                country = countries.get(i);
+                String country_string =
+                        String.format("%-4s %-53s %-15s %-27s %-12s %-36s",
+                                country.code, country.name, country.continent, country.region, country.population, country.capital);
+                System.out.println(country_string);
+            }
+            System.out.println("");
         }
-        System.out.println("");
     }
 
     /**
-     * Gets all countries (default world, pass cont or region to get relevant report).
-     * @return A list of all countries, or null if there is an error.
+     * Gets all countries (defaults to world, pass "c" (continent) or "r" (region) to get relevant report)
+     * 'choice' is only used if continent or region is specified as reportType
+     * @param reportType should be "w", "c" or "r", "" can also be used to get world
+     * @param choice if selecting a continent or region, this should be specified here - ignored if report type is "w"
+     * @return A list of all countries, or null if there is an error
      */
-    public ArrayList<Country> getAllCountries(String cont, String region)
+    public ArrayList<Country> getAllCountries(String reportType, String choice)
     {
         try
         {
             // Create an SQL statement
             Statement stmt = con.createStatement();
+
+            // Checks report type valid and correctly sets formatting
+            if(reportType.toUpperCase().equals("C")){
+                reportType = "Continent";
+            } else if (reportType.toUpperCase().equals("R")){
+                reportType = "Region";
+            } else if (reportType.toUpperCase().equals("W") || reportType.equals("")){
+                reportType = "World";
+            } else {
+                System.out.println("Countries report type not valid\n");
+                return null;
+            }
+
             // Create string for SQL statement
             String strSelect =
-                    "SELECT Code, Name, Population, Continent, Region, Capital "
-                            + "FROM country"
-                           /* if (!cont.equals("")){
-                                + "WHERE Continent = " + cont;
-                            } else if (!region.equals("")){
-                                + "WHERE Region = " + region;
-                            }*/
-                            + " ORDER BY Population DESC";
+                    "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as Capital\n"
+                           + "FROM country\n"
+                           + "LEFT JOIN city on country.Capital = city.ID\n";
+            // Sets where clause for continent or region
+            if(!(reportType.equals("World"))){
+                strSelect += " WHERE " + reportType + " = '" + choice + "'\n";
+            }
+            // Orders by largest population to smallest
+            strSelect += " ORDER BY Population DESC";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
-            // Extract employee information
+            // Extract country information from result set
             ArrayList<Country> countries = new ArrayList<Country>();
             while (rset.next())
             {
                 Country country = new Country();
                 country.code = rset.getString("Code");
                 country.name = rset.getString("Name");
-                country.population = rset.getInt("Population");
                 country.continent = rset.getString("Continent");
                 country.region = rset.getString("Region");
+                country.population = rset.getInt("Population");
                 country.capital = rset.getString("Capital");
                 countries.add(country);
             }
